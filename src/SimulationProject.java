b2bfe1e1e03525e7d29deda17f9936a596dcdbfd
cosmicblaze454 <61728898+cosmicblaze454@users.noreply.github.com>
2022-12-05 18:14:19 +0800

    update

--- src/SimulationProject.java
+++ src/SimulationProject.java
@@ -8,11 +8,12 @@
 import acm.program.*;
 public class SimulationProject extends ConsoleProgram {
     private int continueValue = 0;
+    private float input;
+    private int exponent;
     public void run() {
         println("IEEE-754 Binary-64 Floating Point Converter");
         while (continueValue != 1 ) {
             operations();
-            int input = readInt();
             switch (input){
                 case 1:
                     binaryMantissa();
@@ -22,15 +23,6 @@ public class SimulationProject extends ConsoleProgram {
                     break;
                 case 3:
                     naN();
-                    break;
-                case 4:
-                    binaryOutput();
-                    break;
-                case 5:
-                    hexadecimalEquivalent();
-                    break;
-                case 6:
-                    outputTextFile();
                     break;
                 case 7:
                     continueValue = 1;
@@ -46,16 +38,22 @@ public class SimulationProject extends ConsoleProgram {
         println("1- Input binary mantissa and base-2 (i.e., 101.01x2^5)");
         println("2- Input decimal mantissa and base-10 (i.e., 15.75x10^5)");
         println("3- Input NaN");
-        println("4- Output binary output with space between section");
-        println("5- Output its hexadecimal equivalent");
-        println("6- Output with option to output in text file");
         print("7- Quit\n>>");
     }
     public void binaryMantissa(){
-        println("TODO 1\n");
+        println("Mantissa: ");
+        input = readFloat();
+        println("Exponent of base-2: ");
+        exponent = readInt();
+        normalize();
     }
     private void decimalMantissa(){
-        println("TODO 2\n");
+        println("Mantissa: ");
+        input = readFloat();
+        println("Exponent of base-10: ");
+        exponent = readInt();
+        convertToBinary();
+        normalize();
     }
     private void naN(){
         println("TODO 3\n");
@@ -68,6 +66,25 @@ public class SimulationProject extends ConsoleProgram {
     }
     private void outputTextFile(){
         println("TODO 6\n");
+    }
+    private void convertToBinary(){
+        //String bin = Integer.toBinaryString(input);
+        //input = Integer.parseInt(bin);
+    }
+    private void normalize(){
+        int firstDigit = Integer.parseInt(Integer.toString(number).substring(0, 1));
+        if (firstDigit == 1){
+            while(floor(input) != 1){
+                input = input / 10;
+                exponent++;
+            }
+            
+        } else if (firstDigit == 0){
+            while(floor(input) != 1){
+                input = input * 10;
+                exponent--;
+            }
+        }
     }
     /* Solves NoClassDefFoundError */
     public static void main(String[] args) {
